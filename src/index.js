6faa33ca07bba9f25c7af091df4f0359ddca22da
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-07-23 09:48:09 +0300

    1

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  < BrowserRouter >
-  <React.StrictMode>
-    <App />
+    <React.StrictMode>
+      < BrowserRouter >
+      <App />
+    </BrowserRouter>  
   </React.StrictMode>
-  </BrowserRouter>
+  
 );
