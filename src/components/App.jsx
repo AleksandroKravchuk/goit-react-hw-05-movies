dc5c4a6ef785873b1109435ce9b2a449f00fde97
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-07-23 09:44:20 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 // import { useState } from 'react';
-import HomePage from './pages/Home/Home';
+// import HomePage from './pages/Home/Home';
 import Header from './Header/Header';
-import Form from './pages/Movies/Movies';
+// import Form from './pages/Movies/Movies';
 
 export const App = () => {
   // const [searchName, setSearchName] = useState('');
@@ -12,11 +12,11 @@ export const App = () => {
   return (
     <>
       <Header></Header>
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<Form />} />
         <Route path="*" element={<HomePage />} />
-      </Routes>
+      </Routes> */}
     </>
   );
 };
